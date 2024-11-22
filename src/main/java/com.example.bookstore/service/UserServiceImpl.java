88f65a70dd43a30139cfce843904e4a465738f22
Ayussh Gupta <72377031+KingAyussh1@users.noreply.github.com>
2024-11-22 10:24:41 +0530

    Create UserServiceImpl.java

---
+++ src/main/java/com.example.bookstore/service/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.bookstore.service;
+
+import com.example.bookstore.model.User;
+import com.example.bookstore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("USER");
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+}
