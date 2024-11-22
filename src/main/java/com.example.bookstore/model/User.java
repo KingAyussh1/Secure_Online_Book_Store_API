9aa4ea2a61d71d9d86b5034464221db63aad6436
Ayussh Gupta <72377031+KingAyussh1@users.noreply.github.com>
2024-11-22 10:10:39 +0530

    Create User.java

---
+++ src/main/java/com.example.bookstore/model/User.java
@@ -0,0 +1,23 @@
+package com.example.bookstore.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;  // Either "USER" or "ADMIN"
+}
